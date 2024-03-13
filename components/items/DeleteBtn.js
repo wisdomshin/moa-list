17542d1b2a975703734f5dfb5d0b3024f7602ec6
shinjye <wmwwm88.dev@gmail.com>
2024-03-14 01:34:37 +0900

    글 삭제 버튼 클릭 시 alert 적용

--- components/items/DeleteBtn.js
+++ components/items/DeleteBtn.js
@@ -1,6 +1,13 @@
 'use client';
 
 export default function DeleteBtn(props) {
+  const onRemove = () => {
+    if (window.confirm('정말 삭제하시겠습니까?')) {
+      alert('삭제되었습니다.');
+    } else {
+      alert('취소 되었습니다.');
+    }
+  };
   return (
     <form action='/api/post/delete' method='POST'>
       <input
@@ -12,6 +19,7 @@ export default function DeleteBtn(props) {
       <button
         type='submit'
         className='px-2 py-1 border border-[#546de5] text-[#546de5] rounded-md'
+        onClick={onRemove}
       >
         삭제
       </button>
